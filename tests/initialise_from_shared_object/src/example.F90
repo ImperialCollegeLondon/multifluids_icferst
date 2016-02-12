91e3734f38b28a58e195ed3c66c9531829911d22
Pablo Salinas <pablo.salinas@imperial.ac.uk>
2016-02-12 10:56:53 +0000

    Cleanup of unused files

--- tests/initialise_from_shared_object/src/example.F90
+++
@@ -1,8 +0,0 @@
-subroutine val(field,X,t)
-  double precision, dimension(:) :: field
-  double precision, dimension(:,:) :: X
-  double precision :: t
-
-  field=X(1,:)**2-X(2,:)
-
-end subroutine val
