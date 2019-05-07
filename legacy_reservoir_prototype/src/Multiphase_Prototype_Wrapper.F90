05a704f94eb0da50c0de76c82ac086da1fbeff1d
pablo-salinas <pablo.salinas@imperial.ac.uk>
2019-05-07 14:37:49 +0100

    Added back the poroelasticity implementation. It is unfinished and un-tested but at least it should not break anything else and it is not very intrusive

--- legacy_reservoir_prototype/src/Multiphase_Prototype_Wrapper.F90
+++ legacy_reservoir_prototype/src/Multiphase_Prototype_Wrapper.F90
@@ -1054,6 +1054,7 @@ contains
         is_porous_media = have_option('/porous_media_simulator') .or. have_option('/is_porous_media')
         is_magma = have_option('/magma_simulator')
         is_flooding = have_option('/flooding_simulator')
+        is_poroelasticity = have_option('/poroelasticity')
         !Flag to set up the coupling with femdem
         is_multifracture = have_option( '/femdem_fracture' )
         !Flag to set up blasting
