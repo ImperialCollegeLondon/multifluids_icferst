5355f384424980e4a5b05ff1b306dbaefb764757
pablo-salinas <pablo.salinas@imperial.ac.uk>
2020-06-30 16:48:37 +0100

    Changed license of ICFERST to GPL 2.1

--- legacy_reservoir_prototype/src/Extract_From_State.F90
+++ legacy_reservoir_prototype/src/Extract_From_State.F90
@@ -1,17 +1,7 @@
-!    Copyright (C) 2006 Imperial College London and others.
-!
-!    Please see the AUTHORS file in the main source directory for a full list
-!    of copyright holders.
-!
-!    Prof. C Pain
-!    Applied Modelling and Computation Group
-!    Department of Earth Science and Engineering
-!    Imperial College London
-!
-!    amcgsoftware@imperial.ac.uk
+!    Copyright (C) 2020 Imperial College London and others.
 !
 !    This library is free software; you can redistribute it and/or
-!    modify it under the terms of the GNU Lesser General Public
+!    modify it under the terms of the GNU General Public
 !    License as published by the Free Software Foundation,
 !    version 2.1 of the License.
 !
@@ -20,7 +10,7 @@
 !    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 !    Lesser General Public License for more details.
 !
-!    You should have received a copy of the GNU Lesser General Public
+!    You should have received a copy of the GNU General Public
 !    License along with this library; if not, write to the Free Software
 !    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
 !    USA
