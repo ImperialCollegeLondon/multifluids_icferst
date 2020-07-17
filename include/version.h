96d0f2b67e855f191dcf6e18861a18a52d8a3693
Geraldine Regnier <gr814@roaming-c8-d9-d2-0f-f4-f0.ic.ac.uk>
2020-07-17 09:19:59 +0100

    Updating to latest master changes 16.07 and including .o files

--- include/version.h
+++ include/version.h
@@ -1,4 +1,4 @@
 #ifndef _FLUIDITY_VERSION_H
 #define _FLUIDITY_VERSION_H
-#define __FLUIDITY_VERSION__ "geraldine_dev:3a27887d55eddc2400538925f20f5ed459823f47"
+#define __FLUIDITY_VERSION__ "geraldine_dev:19f695a3bf34bf76f74344a0481de54fdda57b0f"
 #endif /* _FLUIDITY_VERSION_H */
