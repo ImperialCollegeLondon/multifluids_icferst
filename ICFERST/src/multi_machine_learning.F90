b487c5dbc59437caf782ea8c12a80bfcdc0e7061
viluiz <viluiz@gmail.com>
2021-11-16 15:46:44 +0000

    Added linking with XGBOOST, dummy case working

--- ICFERST/src/multi_machine_learning.F90
+++ ICFERST/src/multi_machine_learning.F90
@@ -18,36 +18,75 @@
 
 module multi_machine_learning
 
-#ifdef USING_XGBOOST
+  use iso_c_binding
   use xgb_interface
-#endif
 
-    use fldebug
-    use state_module
-    use fields
-    use field_options
-    use spud
-    use parallel_tools
-    use global_parameters
-    use futils, only: int2str
-    use boundary_conditions_from_options
-    use parallel_tools, only : allmax, allmin, isparallel, getprocno
-    use parallel_fields
-    use memory_diagnostics
-    use initialise_fields_module, only: initialise_field_over_regions
-    use halos
-    use multi_data_types
-    use multi_tools
-
-    implicit none
+  implicit none
 
   contains
 
-    subroutine init_xgboost()
-        implicit none
-        print *, "Testing xgboost"
-      end subroutine init_xgboost
+  subroutine init_xgboost()
 
-      
+    implicit none
+    integer                    :: error   
+    type(c_ptr)                :: xgb_model
+    type(c_ptr)                :: dmatrix         
+    real(c_float), allocatable :: xgb_input(:)    
+    type(c_ptr)                :: out_result_c
+    integer(c_int64_t)         :: out_len 
+    real(c_float), pointer     :: out_result(:)
+    
+    ! Constants
+    integer(c_int)             :: option_mask = 0
+    integer(c_int)             :: ntree_limit = 0
+    real(c_float), parameter   :: missing = -999.0
+    integer(c_int64_t)         :: dmatrix_len = 0
+    integer(c_int64_t)         :: nrow = 1
+    integer(c_int64_t)         :: ncol = 17 
+    character(len=255)         :: name_xgb_model = 'XGBRFmodel_orig_lessfeatures_xgbformat.bin'
+    
+    ! Local variables
+    logical :: there = .false.
+    real(c_float), dimension(17) :: mean, std, raw_input, norm_input 
 
+    !--- Initialize variables
+    mean = (/4.606498E+00, 4.134381E+01, 1.312609E+01, 1.869254E-02, 7.138944E+02, 3.206514E-06, 1.012754E+00, 3.133900E-03, 8.245164E-02, 9.482336E-02, 1.752161E-01, 2.600834E+00, -2.709370E+09, 2.655941E-06, 1.865938E-05, 8.406276E-01, 1.148470E+01 /)
+    std = (/1.685133E+00, 2.110226E+03, 6.861566E+02, 2.188420E-02, 1.111934E+03, 9.776763E-06, 2.756072E+00, 1.341650E-02, 4.978913E-01, 3.173449E-01, 1.437997E+00, 9.703361E+00, 3.664636E+11, 6.481185E-05, 1.378213E-03, 9.822488E+01, 1.795359E+01 /)
+    raw_input = (/5.000000E+00, 4.890481E+00, 3.937589E-01, 2.464229E-02, 2.388089E+02, 3.333715E-07, 9.755054E-01, 2.556481E-03, 6.391201E-02, 1.238833E-01, 3.023031E-01, 3.097082E+00, -1.180094E+06, 5.637608E-08, 1.044071E-07, 5.399639E-01, 3.000000E+00 /)
+    norm_input = (raw_input - mean)/std 
+
+    !--- Starts here
+    write(*,*) 'Starting XGBoost program'
+    inquire( FILE=name_xgb_model, EXIST=there) 
+    if ( .not. there ) write(*,*) 'Machine learning model do not exist!'
+    
+    !!! Load XGB model !!!
+    ! Create  a dummy  XGDMatrix 
+    allocate(xgb_input(ncol))
+    xgb_input(:) = 0.0
+    error = fortran_XGDMatrixCreateFromMat(xgb_input, nrow, ncol, missing, dmatrix)
+    ! Create XGBooster object
+    error = fortran_XGBoosterCreate(dmatrix, dmatrix_len, xgb_model)
+    ! Load XGBooster model from binary file
+    write(*,*) 'Reading '//trim(name_xgb_model)
+    ! Always use "trim(name)//c_null_char" to pass the file name 
+    error = fortran_XGBoosterLoadModel(xgb_model, trim(name_xgb_model)//c_null_char)
+
+    
+    !!! Make predictions !!!
+    ! Create XGDMatrix using the input values 
+    xgb_input = norm_input
+    error = fortran_XGDMatrixCreateFromMat(xgb_input, nrow, ncol, missing, dmatrix)
+    ! Make prediction. The result will be stored in c pointer out_result_c 
+    error = fortran_XGBoosterPredict(xgb_model, dmatrix, option_mask, ntree_limit, out_len, out_result_c)
+    ! Link to fortran pointer out_result 
+    call c_f_pointer(out_result_c, out_result, [out_len])
+    write(*,*) 'XGB model Prediction: ',out_result
+    
+    !--- Cleanup
+    error = fortran_XGBoosterFree(xgb_model)
+    deallocate(xgb_input)
+    nullify(out_result)
+  
+  end subroutine init_xgboost
 end module multi_machine_learning
---
+++ ICFERST/src/xgb_interface.F90
@@ -0,0 +1,80 @@
+!----------------------------------------------------------------------------------------
+!> @author Vinicius L S Silva
+!> @brief Interface to call XGBoost library C API from fortran
+!> This code is a modification of https://github.com/christophkeller/fortran2xgb 
+!----------------------------------------------------------------------------------------
+
+module xgb_interface
+
+   use iso_c_binding
+
+   implicit none
+
+   interface
+   ! Create matrix content from dense matrix
+   ! Returns 0 when success, -1 when failure happens
+      integer(c_int) function fortran_XGDMatrixCreateFromMat(data, nrow, ncol, missing, out) bind(c, name="XGDMatrixCreateFromMat")
+         use iso_c_binding, only: c_int, c_ptr, c_float, c_int64_t
+         ! Parameters
+         real(c_float)             :: data(*) ! Pointer to the data space
+         integer(c_int64_t), value :: nrow    ! Number of rows
+         integer(c_int64_t), value :: ncol    ! Number columns
+         real(c_float), value      :: missing ! Which value to represent missing value
+         type(c_ptr)               :: out     ! Created dmatrix
+         ! End parameters 
+      end function
+   end interface    
+
+   interface
+   ! Create xgboost learner
+   ! Returns 0 when success, -1 when failure happens
+      integer(c_int) function fortran_XGBoosterCreate(dmats, len, out) bind(c, name="XGBoosterCreate")
+         use iso_c_binding, only: c_int, c_ptr, c_int64_t
+         ! Parameters
+         type(c_ptr), value        :: dmats ! Matrices that are set to be cached
+         integer(c_int64_t), value :: len   ! Length of dmats
+         type(c_ptr)               :: out   ! Handle to the result booster
+         ! End parameters 
+      end function
+   end interface   
+   
+   interface
+   ! Load model from existing file.
+   ! Returns 0 when success, -1 when failure happens
+      integer(c_int) function fortran_XGBoosterLoadModel(handle, fname) bind(C, name="XGBoosterLoadModel")
+         use iso_c_binding, only: c_int, c_char, c_ptr
+         ! Parameters
+         type(c_ptr), value                          :: handle ! Handle
+         character(len=1, kind=c_char), dimension(*) :: fname  ! File URI or file name (*needs to be in the right c format - use trim(name_in_fortran)//c_null_char)
+         ! End parameters 
+      end function
+   end interface
+   
+   interface
+   ! Make prediction based on dmatrix
+   ! Returns 0 when success, -1 when failure happens
+      integer(c_int) function fortran_XGBoosterPredict(handle, dmat, option_mask, ntree_limit, out_len, out_result) bind(C, name="XGBoosterPredict")
+         use iso_c_binding, only: c_int, c_ptr, c_int64_t
+         ! Parameters
+         type(c_ptr), value        :: handle       ! Handle
+         type(c_ptr), value        :: dmat         ! Data matrix
+         integer(c_int), value     :: option_mask  ! Bit-mask of options taken in prediction, possible values 0:normal prediction 1:output margin instead of transformed value 2:output leaf index of trees instead of leaf value, note leaf index is unique per tree 4:output feature contributions to individual predictions
+         integer(c_int), value     :: ntree_limit  ! Limit number of trees used for prediction, this is only valid for boosted trees when the parameter is set to 0, we will use all the trees
+         integer(c_int64_t)        :: out_len      ! Used to store length of returning result
+         type(c_ptr)               :: out_result   ! Used to set a pointer to array
+         ! End parameters 
+      end function
+   end interface
+   
+   interface
+   ! Free obj in handle
+   ! Returns 0 when success, -1 when failure happens
+      integer(c_int) function fortran_XGBoosterFree(handle) bind(c, name="XGBoosterFree")
+         use iso_c_binding, only: c_int, c_ptr
+         ! Parameters
+         type(c_ptr), value :: handle ! Handle to be freed
+         ! End parameters 
+      end function
+   end interface
+   
+end module
