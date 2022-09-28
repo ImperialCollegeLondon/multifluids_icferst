7e2ddd1408120a6214dcee6b62410b48e53063d3
Hayley Firth <hf116@ese-potato>
2022-09-28 16:17:08 +0100

    updating dynamic stop for halite optimiser

--- ICFERST/src/multi_pipes.F90
+++ ICFERST/src/multi_pipes.F90
@@ -2825,7 +2825,7 @@ contains
         !Initialize gamma
         sfield=>extract_scalar_field(state(1),"Gamma",ipres)
         ! This is for Halite optimiser to dynamically stop simulations
-        if (minval(sfield%val)==-666) STOP 11011998
+        if (maxval(sfield%val)- 666 < 1e-5) STOP 11011998 
         
         pipes_aux%GAMMA_PRES_ABS = 0.0
         do ipres = 1, Mdims%npres
