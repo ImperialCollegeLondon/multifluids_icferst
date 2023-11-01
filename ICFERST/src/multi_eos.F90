ImperialCollegeLondon/multifluids_icferst

commit c3622a770ba1d4463ed6a7d6f774c091dd796579

Bugfix for nondebugging not having defined NFIELD for fast options. For not anisotropic perm avoid inversion of the tensorial permeability and just invert diagonals

ICFERST/src/multi_eos.F90
@@ -807,10 +807,15 @@ contains
 
        !Obtain inverse of permeability and store it
        !SPRINT_TO_DO THIS COULD BE DONE FASTER IF WE KNOW IF IT HAS OFF DIAGONALS OR NOT
-       do i = 1, size(perm%val,3)
-           upwnd%inv_permeability( :, :, i)=inverse(perm%val( :, :, i))
-       end do
-
+       if (has_anisotropic_permeability) then 
+        do i = 1, size(perm%val,3)
+          upwnd%inv_permeability( :, :, i)=inverse(perm%val( :, :, i))
+        end do
+       else 
+        forall (j = 1:size(perm%val,1), i = 1:size(perm%val,3)) 
+          upwnd%inv_permeability( j, j, i) = 1./perm%val( j, j, i)
+        end forall
+       end if
        allocate(viscosities(Mdims%nphase, Mdims%cv_nonods))
        DO IPHASE = 1, Mdims%nphase!Get viscosity for all the phases
         option_path_python = "/material_phase["// int2str( iphase - 1 )//"]/phase_properties/Viscosity/tensor_field"//&

ICFERST/src/cv-adv-dif.F90
@@ -687,8 +687,8 @@ contains
 
           IF ( GOT_T2 .OR. THERMAL) call get_var_from_packed_state( packed_state, &
               PhaseVolumeFraction = T2_ALL, OldPhaseVolumeFraction = T2OLD_ALL )
-
-
+          !Initialise NFIELD
+          NFIELD=0
           if (activate_limiters) then 
             ! FOR packing as well as for detemining which variables to apply interface tracking**********
             !          STORE=.TRUE.
@@ -727,7 +727,6 @@ contains
                 ENDIF
             END DO
 
-            NFIELD=0
             DO IFI=1,size(IGOT_T_PACK,2)
                 DO IPHASE=1,final_phase
                     IF(.not.IGOT_T_CONST(IPHASE,IFI)) NFIELD=NFIELD+1

commit 1db7589b75f2cd69fb5d3b4b60e1420be617eec9

Bugfix repeatability
Simulations produced random non-repeatability issues introduced since commit: c3622a770ba1d4463ed6a7d6f774c091dd796579
Reverting part of the changes introduced then.

ICFERST/src/multi_eos.F90
@@ -878,15 +878,10 @@ contains
 
        !Obtain inverse of permeability and store it
        !SPRINT_TO_DO THIS COULD BE DONE FASTER IF WE KNOW IF IT HAS OFF DIAGONALS OR NOT
-       if (has_anisotropic_permeability) then 
-        do i = 1, size(perm%val,3)
-          upwnd%inv_permeability( :, :, i)=inverse(perm%val( :, :, i))
-        end do
-       else 
-        forall (j = 1:size(perm%val,1), i = 1:size(perm%val,3)) 
-          upwnd%inv_permeability( j, j, i) = 1./perm%val( j, j, i)
-        end forall
-       end if
+       do i = 1, size(perm%val,3)
+           upwnd%inv_permeability( :, :, i)=inverse(perm%val( :, :, i))
+       end do
+
        allocate(viscosities(Mdims%nphase, Mdims%cv_nonods))
        DO IPHASE = 1, Mdims%nphase!Get viscosity for all the phases
         option_path_python = "/material_phase["// int2str( iphase - 1 )//"]/phase_properties/Viscosity/tensor_field"//&