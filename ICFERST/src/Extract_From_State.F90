04a261f9435d92e798ca2b34130c7ca0f2dda299
Meissam Bahlali <m.bahlali@imperial.ac.uk>
2023-11-21 13:38:41 +0000

    Forgot to add the coefficients field to state for the partition coefficient method

--- ICFERST/src/Extract_From_State.F90
+++ ICFERST/src/Extract_From_State.F90
@@ -599,7 +599,7 @@ contains
         type(vector_field), pointer :: velocity, position, vfield, ldvfield, tdvfield
         type(tensor_field), pointer :: tfield, p2, d2, drhodp
         type(vector_field) :: porosity, vec_field, porous_density, porous_density_initial, porous_heat_capacity, &
-             Longitudinal_Dispersivity, Transverse_Dispersivity, porous_density_old
+             Longitudinal_Dispersivity, Transverse_Dispersivity, porous_density_old, K_A, K_const, K_c, K_T, K_c2, K_T2, K_cT
         type(vector_field) :: p_position, u_position, m_position
         type(tensor_field) :: permeability, ten_field, porous_thermal_conductivity
         type(mesh_type), pointer :: ovmesh, element_mesh
